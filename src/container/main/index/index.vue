0219d6c5ca0214e2e9f2debf51fa1d85cca865cf
crazylxr <1076629390@qq.com>
2018-04-16 19:52:24 +0800

    搜索
    搜索

--- src/container/main/index/index.vue
+++ src/container/main/index/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="indexContainer">
+    <el-card style="text-align: center;">
+      <el-form :inline="true" :model="searchForm" class="searchForm">
+        <el-form-item label="标题：">
+          <el-input v-model="searchForm.title" placeholder="标题"></el-input>
+        </el-form-item>
+        <el-form-item label="价格:">
+          <el-input v-model="searchForm.startPrice"></el-input>
+        </el-form-item>
+        <el-form-item label="~">
+          <el-input v-model="searchForm.endPrice"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
     <el-card>
       <div class="goodsList">
         <div class="goodsItem" v-for="(item, index) in data.content" :key="index">
@@ -18,7 +35,12 @@ import { getIndexGoods } from '../../../api/main/indexApi'
 export default {
   data () {
     return {
-      data: {}
+      data: {},
+      searchForm: {
+        title: '',
+        startPrice: 0,
+        endPrice: 100
+      }
     }
   },
 

21d07d7c7f7561f9c950849e0b862220a48349f9
crazylxr <1076629390@qq.com>
2018-04-23 22:13:03 +0800

    详情页

--- src/container/main/goodsDetail/index.vue
+++ src/container/main/goodsDetail/index.vue
@@ -1,16 +1,276 @@
 <template>
     <div id="goodsDetail">
-        <div class="major">
-            <div class="gallery">
-                <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+        <div class="product-info">
+            <div class="preview-wrap">
+                <div class="preview">
+                    <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+                </div>
+                <div class="spec-list">
+                    <ul>
+                        <li>
+                            <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+                            <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+                            <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="detail">
-                <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+            <div class="itemInfo-wrap">
+                <h3>惠普（HP）战86 商用办公台式电脑整机（i3-7100 4G 1TB 集显 Win10 光驱 三年上门）19.5英寸</h3>
+
+                <div class="summary-price-wrap">
+                    <div class="summary-price">
+                        <span class="property-type">原价</span>
+                        <span class="property-cont">￥1000</span>
+                    </div>
+                    <div class="discount-price">
+                        <span class="property-type">折扣价</span>
+                        <span class="property-cont">￥800</span>
+                    </div>
+                </div>
+
+                <div class="inventory">
+                    <span class="property-type">库存</span>
+                    <span class="property-cont">800</span>
+                </div>
+
+                <div class="spec">
+                    <span class="property-type">规格</span>
+                    <ul>
+                        <li>
+                            <el-button size="mini">规格1</el-button>
+                        </li>
+                        <li>
+                            <el-button size="mini">规格2</el-button>
+                        </li>
+                        <li>
+                            <el-button size="mini">规格3</el-button>
+                        </li>
+                        <li>
+                            <el-button size="mini">规格4</el-button>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="amount">
+                    <span class="property-type">数量</span>
+                    <el-input-number size="mini"></el-input-number>
+                </div>
+
+                <div class="action">
+                    <el-button type="primary">立即购买</el-button>
+                    <el-button type="success">加入购物车</el-button>
+                </div>
             </div>
+
         </div>
 
-        <div class="minor">
-
+        <div class="introduce">
+            <div class="toolbar">
+                <el-tabs type="border-card">
+                    <el-tab-pane label="商品介绍">
+                        <div class="description-text">
+                            <p>
+                                品牌: UDON/联顿CPU系列: Intel/英特尔至强E5CPU型号: L5640主板结构: E-ATX主板芯片组类型: Intel X79显卡系列: GTX 1050显存容量: 4GB内存容量: 8GB 16GB硬盘类型: 固态硬盘(SSD)硬盘容量: 120GB 500GB机箱类型: 中塔电源功率: 350W电源80 PLUS认证: 金牌散热方式: 风冷光驱类型: 无光驱声卡接口类型: PCI套餐类型: 套餐一 套餐二 套餐三 套餐四 套餐五 套餐六显存位宽: 128bit显存类型: GDDR5CPU主频: 2.8GHz(含)-3.0GHz(不含)内存类型: DDR3CPU核心数: 八核心内存频率: 1333MHz显卡品牌: 精影主板品牌: zillion/杰灵内存品牌: 镁光硬盘品牌: WD/西部数据电源品牌: ICE机箱品牌: ICE散热设备品牌: pccooler/超频三机箱结构: ATX配置类型: 疯狂游戏型显卡类型: 独立显卡
+                            </p>
+                        </div>
+                        <div class="description-imgs">
+                            <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+                            <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+                            <img src="http://localhost:8081//c9e53db5-ee3b-4493-b168-24f725a9aa9b.png" alt="">
+                        </div>
+                    </el-tab-pane>
+                    <el-tab-pane label="商品评价">
+                        <ul class="comment">
+                            <li>
+                                <div class="user">
+                                    <img class="avtor" src="//wwc.alicdn.com/avatar/getAvatar.do?userIdStr=vFkSPFHuXH*evF8LPmHSXHP-MCM-MGl-XFcyPGgyM08WvFNuOHQ4MFvYPm*HXFhh&width=40&height=40&type=sns" alt="">
+                                    <span class="nickname">勿忘心安111</span>
+                                </div>
+                                <div class="content">
+                                    <p>
+                                        收到货后自己动手把老电脑上的1TG机械硬盘拆下，装到了新主机里做存储盘用，这就解决了120G固态硬盘小的问题。主机配置不错，非常平稳流畅，千把元能淘到这样好的宝贝非常满意，在这里对店主表示深切地感谢！全5分好评！！！！！ 收到货后自己动手把老电脑上的1TG机械硬盘拆下，装到了新主机里做存储盘用，这就解决了120G固态硬盘小的问题。主机配置不错，非常平稳流畅，千把元能淘到这样好的宝贝非常满意，在这里对店主表示深切地感谢！全5分好评！！！！！
+                                    </p>
+                                    <p class="time">
+                                        2018年04月17日 18:12
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="user">
+                                    <img class="avtor" src="//wwc.alicdn.com/avatar/getAvatar.do?userIdStr=vFkSPFHuXH*evF8LPmHSXHP-MCM-MGl-XFcyPGgyM08WvFNuOHQ4MFvYPm*HXFhh&width=40&height=40&type=sns" alt="">
+                                    <span class="nickname">勿忘心安111</span>
+                                </div>
+                                <div class="content">
+                                    <p>
+                                        收到货后自己动手把老电脑上的1TG机械硬盘拆下，装到了新主机里做存储盘用，这就解决了120G固态硬盘小的问题。主机配置不错，非常平稳流畅，千把元能淘到这样好的宝贝非常满意，在这里对店主表示深切地感谢！全5分好评！！！！！ 收到货后自己动手把老电脑上的1TG机械硬盘拆下，装到了新主机里做存储盘用，这就解决了120G固态硬盘小的问题。主机配置不错，非常平稳流畅，千把元能淘到这样好的宝贝非常满意，在这里对店主表示深切地感谢！全5分好评！！！！！
+                                    </p>
+                                    <p class="time">
+                                        2018年04月17日 18:12
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </el-tab-pane>
+                </el-tabs>
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {
+    console.log(this.$route.params.id)
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+#goodsDetail {
+  width: 800px;
+  margin: 0 auto;
+}
+.product-info {
+  display: flex;
+}
+
+.itemInfo-wrap {
+  width: 390px;
+  padding: 0 20px;
+}
+
+.preview-wrap {
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+}
+.preview img {
+  width: 100%;
+  border: 1px solid #cecece;
+}
+.spec-list ul {
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+
+  li img {
+    width: 80px;
+    border: 1px solid #999;
+  }
+}
+
+.summary-price-wrap {
+  padding: 10px;
+  background: #f3f3f3;
+  color: #333;
+}
+
+.property-type {
+  display: inline-block;
+  width: 60px;
+  font-size: 14px;
+}
+.summary-price {
+  margin-bottom: 10px;
+  .property-cont {
+    font-weight: 800;
+    text-decoration: line-through;
+  }
+}
+.discount-price {
+  .property-cont {
+    color: #007aaa;
+    font-size: 22px;
+  }
+}
+.inventory {
+  margin-top: 10px;
+  padding-left: 10px;
+}
+.amount {
+  margin-top: 10px;
+  padding-left: 10px;
+  vertical-align: middle;
+}
+
+.action {
+  margin-top: 40px;
+  text-align: center;
+}
+.spec {
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding-left: 0px;
+  }
+
+  li {
+    display: inline-block;
+    margin: 5px;
+  }
+}
+
+.description-imgs {
+  img {
+    width: 100%;
+  }
+}
+
+.comment {
+  display: flex;
+  list-style: none;
+  flex-direction: column;
+
+  li {
+    display: flex;
+    padding: 15px 0;
+    border-bottom: 1px solid #cecece;
+  }
+
+  li:nth-child(1) {
+    padding-top: 0;
+  }
+
+  .user {
+    img {
+      width: 40px;
+      height: 40px;
+    }
+
+    .nickname {
+      display: inline-block;
+      width: 40px;
+      overflow: hidden;
+      font-size: 12px;
+      word-break: keep-all;
+      text-overflow: ellipsis;
+    }
+  }
+
+  .content {
+    padding-left: 10px;
+    font-size: 13px;
+    letter-spacing: 1px;
+    color: #444;
+
+    p {
+      margin: 0;
+    }
+    .time {
+      margin-top: 10px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+}
+</style>
